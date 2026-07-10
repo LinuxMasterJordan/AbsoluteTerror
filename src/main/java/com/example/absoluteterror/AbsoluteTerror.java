8166df9ceec60c71196019e4989c6c1ea4b3add4
LinuxMasterJordan <vansballies@gmail.com>
2026-07-10 20:53:46 +0200

    Initialize AbsoluteTerror mod and register entities

--- src/main/java/com/example/absoluteterror/AbsoluteTerror.java
+++ src/main/java/com/example/absoluteterror/AbsoluteTerror.java
@@ -1,1 +1,19 @@
 
+package com.example.absoluteterror;
+
+import com.example.absoluteterror.entity.ModEntities;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(AbsoluteTerror.MOD_ID)
+public class AbsoluteTerror {
+    public static final String MOD_ID = "absoluteterror";
+
+    public AbsoluteTerror() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModEntities.ENTITY_TYPES.register(bus);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
